f87914025c4a51c1f4aace6b7f72e5c63f3e196f
lukaszplac <lukasz.plac@gmail.com>
2017-12-26 18:19:17 +0100

    link to remote mongo db added

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  // mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://lukasz:parowakaczka@ds133127.mlab.com:33127/kanban',
   port: process.env.PORT || 8000,
 };
 
